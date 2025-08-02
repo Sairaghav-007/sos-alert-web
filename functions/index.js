623370f2805b019ff78a4cf651f250336d23153d
AadithyaSJ <aadithyasuresh2725@gmail.com>
2025-08-02 21:24:49 +0530

    Cleaned initial commit without secrets

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,9 @@ require("dotenv").config(); // Load environment variables
 
 admin.initializeApp();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const fromPhone = process.env.TWILIO_PHONE_NUMBER;
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const fromPhone = process.env.TWILIO_PHONE_NUMBER;
 
 const client = twilio(accountSid, authToken);
 
